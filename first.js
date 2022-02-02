8354b6e50795ec4aa47f5d9612a9ce7e0a568392
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-02 18:58:39 +0600

    celcius to farenheit

--- first.js
+++ first.js
@@ -7,4 +7,13 @@ for(var i = 0; i < sentence.length; i++){
     count++;
   }
 }
-console.log(count)
+// console.log(count);
+
+// celcius to farenheit
+// (0°C × 9/5) + 32 = 32°F
+// first
+var cel = 100; 
+var farenheit = (cel* 9 / 5) + 32;
+console.log(farenheit);
+
+
